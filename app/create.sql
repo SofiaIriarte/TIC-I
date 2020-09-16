2c246a5544ea859f8f33d9a3688bf738a24f8281
BrunoCostanzo04 <bcostanzo@correo.um.edu.uy>
2020-09-15 21:24:04 -0300

    Registro de un cliente

---
+++ app/create.sql
@@ -0,0 +1,10 @@
+
+    create table client (
+       ci varchar(255) not null,
+        address varchar(255),
+        first_name varchar(255),
+        last_name varchar(255),
+        passworld varchar(255),
+        user_name varchar(255),
+        primary key (ci)
+    ) engine=InnoDB
