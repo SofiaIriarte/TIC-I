85918037029900093409b1645249a384de5cfd30
rossivalles <rvalles@correo.um.edu.uy>
2020-11-03 19:35:55 -0300

    about us

---
+++ LaboratorioTic1/Client/src/main/java/um/edu/tic1/client/controllers/AboutUs.java
@@ -0,0 +1,63 @@
+package um.edu.tic1.client.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxWeaver;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import static um.edu.tic1.client.UserApplication.springContext;
+
+@Component
+@FxmlView("/um.edu.tic1.client/aboutUs.fxml")
+public class AboutUs implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML private Button registrarse2;
+    @FXML private Button registrarse;
+    @FXML private Button logIn;
+    @FXML private Button aboutUs;
+    @FXML private Button shop;
+
+    @FXML
+    public void goToRegisterScene (ActionEvent event) throws IOException {
+        FxWeaver fxWeaver = springContext.getBean(FxWeaver.class);
+        Parent root = fxWeaver.loadView(RegistrarUsuario.class);
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void goToLogIn (ActionEvent event) {
+        FxWeaver fxWeaver = springContext.getBean(FxWeaver.class);
+        Parent root = fxWeaver.loadView(LogInUsuario.class);
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void goToShop (ActionEvent event) {
+        FxWeaver fxWeaver = springContext.getBean(FxWeaver.class);
+        Parent root = fxWeaver.loadView(ShopSinLogIn.class);
+        Scene scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
